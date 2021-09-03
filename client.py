b0b5b2e62564494cd29032550af0a9e7aa3bab46
Saketh Reddy <sakethreddy997@gmail.com>
2021-09-03 18:07:20 -0400

    list datatype practice

--- client.py
+++ client.py
@@ -1,14 +1,27 @@
-#single line
-#second line comment
+# single line
+# second line comment
 
 
-
-x=10.25
+x = 10.25
 print(x)
 
-
-y="saketh"
+y = "saketh"
 print(y)
 print(len(y))
 print(y[5])
-#string count starts from 0
+
+# string count starts from 0
+
+print(y[-3])
+# when it comes to counting in opposite direction then it starts from 1 only
+
+psr = [1.34, 3.4, 1.34, 'psry']
+
+psr.append(130)
+psr.insert(4,'sunny')
+
+print(psr.reverse())
+print(psr[2])
+print(psr)
+
+#Practiced strings and list datatypes
